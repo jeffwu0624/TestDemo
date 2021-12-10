37e65bda01b9b4d336d694a16a5e417402a3b39d
Jeff2_Wu <Jeff2_Wu@asus.com>
2021-12-10 13:11:49 +0800

    upgrade dll

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,4 +1,9 @@
+using System.Security.Cryptography.X509Certificates;
+using FluentAssertions;
+using Moq;
+using NSubstitute;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 namespace TestDemo
 {
@@ -7,12 +12,45 @@ namespace TestDemo
         [SetUp]
         public void Setup()
         {
+            
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var mock = new Mock<IMockDemo>();
+
+            mock.Setup(x => x.CreateNumber()).Returns(6);
+            
+            //var mock = Substitute.For<IMockDemo>();
+
+            //mock.CreateNumber().Returns(6);
+
+            var sutDemo = new SutDemo(mock.Object);
+
+            Assert.AreEqual(12, sutDemo.CalNumber());
+
+            sutDemo.CalNumber().Should().Be(12);
+        }
+    }
+
+    public interface IMockDemo
+    {
+        int CreateNumber();
+    }
+
+    public class SutDemo
+    {
+        private readonly IMockDemo _mock;
+
+        public SutDemo(IMockDemo mock)
+        {
+            _mock = mock;
+        }
+
+        public int CalNumber()
+        {
+            return _mock.CreateNumber() * 2;
         }
     }
 }
